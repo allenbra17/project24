cfb139a587c7f8e5c2180111aa232e7b58bde941
allenbra17 <allenbra17@gmail.com>
2022-02-08 09:41:46 -0500

    added NASA and location get

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NasaImage from "./NASA";
+
 
 function App() {
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  function success(pos) {
+    var crd = pos.coords;
+    setLat(crd.latitude);
+    setLong(crd.longitude);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <NasaImage 
+        lat={lat}
+        long={long}/>
       </header>
     </div>
   );
